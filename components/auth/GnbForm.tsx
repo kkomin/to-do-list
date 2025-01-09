35cdceed02a4eeae22ecabe9a1b16f7d73986b88
kkomin <100822055+kkomin@users.noreply.github.com>
2025-01-10 02:19:58 +0900

    [DESIGN] : 🎨 GnbForm 생성 및 디자인 설정

---
+++ components/auth/GnbForm.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { useEffect, useState } from "react";
+
+export default function GnbForm() {
+    const [isMobile, setIsMobile] = useState(false);
+    
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 767);
+        };
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    })
+
+    return (
+        <div className="w-full h-[60px] flex justify-start items-start border-b lg:border-gray-200">
+            <img className="w-[71px] h-10 relative top-[9.71px] ml-[16px] 
+            sm:w-auto sm:ml-[24px] lg:w-[151px] lg:ml-[360px] object-fit: contain"
+            src={isMobile ? '/img/image.png' : '/img/doit.png'}/>
+        </div>
+    );
+}
