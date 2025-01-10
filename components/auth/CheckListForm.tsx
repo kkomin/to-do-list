b0629f6d901c941a293d1469ee0ee3382aa2bffc
kkomin <100822055+kkomin@users.noreply.github.com>
2025-01-10 20:57:19 +0900

    [FEATURE] : ⚙️ CheckList 클릭 시 이벤트 설정

--- components/auth/CheckListForm.tsx
+++ components/auth/CheckListForm.tsx
@@ -1,12 +1,21 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
+// 클릭 시 변해야 하는 요소 => 1. 배경 color purple-200 .. 2. 체크된 icon으로 .. 3. text에 취소선
 export function CheckListForm() {
+    const [isChecked, setIsChecked] = useState(false);
+    
+    // 이벤트 헨들러 생성
+    const handleClick = () => {
+        setIsChecked(!isChecked);
+    };
+    
     return (
-        <div className="w-[344px] h-[50px] relative top-[21px] flex items-center gap-[10px] rounded-full bg-white border-solid border-2 border-slate-900 px-3
-        sm:w-[696px] lg:w-[588px]">
-            <img src="/ic/checkbox_empty.png" className="w-8 h-8"/>
-            <span className="text-slate-800 text-center">비타민 챙겨 먹어라</span>
+        <div className={`w-[344px] h-[50px] relative top-[21px] flex items-center gap-[10px] rounded-full bg-white border-solid border-2 border-slate-900 px-3
+        sm:w-[696px] lg:w-[588px] ${isChecked ? "bg-purple-200" : "bg-white"}`}onClick={handleClick}>
+            <img src={isChecked ? "ic/checkbox.png" : "/ic/checkbox_empty.png"} className="w-8 h-8"/>
+            <span className={`text-slate-800 text-center
+                ${isChecked ? "line-through text-slate-800" : ""}`}>비타민 챙겨 먹어라</span>
         </div>
     );
 }
