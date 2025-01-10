2e0850c6930f89bfe2b96b839ea79ae78415c734
kkomin <100822055+kkomin@users.noreply.github.com>
2025-01-10 14:40:28 +0900

    [DESIGN] : 🎨 SearchBar 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import GnbForm from "@/components/auth/GnbForm";
+import GnbForm from "@/components/GnbForm";
+import { SearchForm } from "@/components/SearchForm";
 
 export default function Home() {
   return (
     <main>
       <GnbForm/>
+      <SearchForm/>
     </main>
   );
 }
---
+++ components/SearchForm.tsx
@@ -0,0 +1,9 @@
+export function SearchForm() {
+    return (
+        <div className="flex absolute top-[76px] sm:top-[84px] lg:top-[84px]">
+            <input alt="searchbar" className="rounded-full bg-slate-100 border-2 border-solid border-b-slate-900 border-b-[6px] border-slate-900 
+                focus:ring-0 pl-[24px] w-[280px] h-[56px] border-input ml-[16px] file:text-sm focus-visible:outline-none 
+                placeholder:text-slate-500 sm:w-[518px] sm:ml-[24px] lg:w-[1016px] lg:ml-[360px]" placeholder="할 일을 입력하세요"/>
+        </div>
+    );
+}
