3d77e88dd90391fa55e8ff9be64cc6bda06339ac
kkomin <100822055+kkomin@users.noreply.github.com>
2025-01-11 16:49:41 +0900

    [DESIGN] : 🎨 전체 페이지 배경 색상 변경

--- components/auth/GnbForm.tsx
+++ components/auth/GnbForm.tsx
@@ -15,7 +15,7 @@ export default function GnbForm() {
     })
 
     return (
-        <div className="w-full h-[60px] flex justify-start items-start border-b lg:border-gray-200">
+        <div className="w-full h-[60px] flex justify-start items-start border-b lg:border-gray-200 bg-white">
             <img className="w-[71px] h-10 relative top-[9.71px] ml-[16px] 
             sm:w-auto sm:ml-[24px] lg:w-[151px] lg:ml-[360px] object-fit: contain"
             src={isMobile ? '/img/image.png' : '/img/doit.png'}/>
