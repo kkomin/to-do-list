28af47c0e821a5a11a5ebe9c7bb3776f50c39aab
kkomin <100822055+kkomin@users.noreply.github.com>
2025-01-13 14:45:42 +0900

    [DESIGN] : 🎨 홈페이지 디자인 재설정

--- app/page.tsx
+++ app/page.tsx
@@ -32,28 +32,27 @@ const TextSection = ({ text, className, isEmpty }: textProps) => {
 
 export default function Home() {
   const [todo, setAddTodo] = useState<string[]>([]);
+  const [done, setDoneLength] = useState<number>(0);
+
   const handleAddTodo = (text:string) => {
     setAddTodo([...todo, text]);
+  }
+  
+  const handleDone = (length:number) => {
+    setDoneLength(length);
   }
 
   return (
     <main>
       <SearchForm onAddTodo={handleAddTodo}/>
-      {/* TO DO 리스트 */}
-      <div className="flex flex-col">
-        <ImageSection
-          src="/img/todo.png"
-          className="w-[101px] h-[36px] mt-[96px] relative ml-[16px] lg:mt-[120px] lg:ml-[360px] sm:mt-[120px] sm:ml-[24px]"
-        />
-        <div className="w-[334px] sm:w-[696px] lg:w-[588px]">
-        <CheckListForm todo={todo}/>
-        </div>
-        <div className="flex flex-col items-center lg:mt-[140px] lg:hidden">
-          <ImageSection
-            src="/img/doit_list.png"
-            className={`w-[120px] h-[120px] mt-[11px] sm:w-[240px] sm:h-[240px]
-              ${todo.length > 0 ? "hidden" : ""}`}
-              />
+      {/* 모바일, 태블릿에서 보기 */}
+      <div className="flex flex-col ml-[16px] sm:ml-[24px] lg:hidden">
+        {/* Todo 그룹 */}
+        <div className="mt-[96px] sm:mt-[120px]">
+          <ImageSection src="/img/todo.png"
+            className="w-[101px] h-[36px]"/>
+          <ImageSection src = "img/doit_list.png" 
+            className="w-[120px] h-[120px] ml-[112px] sm:w-[240px] sm:h-[240px] sm:ml-[228px]"/>
           <TextSection
             text= {
               <div>
@@ -61,77 +60,67 @@ export default function Home() {
               TODO를 새롭게 추가해주세요!
               </div>
             }
-            className="text-slate-400 text-base text-center mt-[16px]"
-            isEmpty={todo.length === 0}/>
-          </div>
-      </div>
+            className="text-slate-400 text-base text-center mt-[16px] ml-[72px] sm:ml-[248px] sm:mt-[24px]"
+            isEmpty={todo.length === 0 && done === 0}/>
+        </div>
 
-      <div className="flex flex-col mt-[48px] lg:hidden ">
-        <ImageSection
-            src="/img/done.png"
-            className="w-[97px] h-[36px] ml-[16px] sm:ml-[24px]"
-            />
-      </div>
-
-      <div className="lg:hidden">
-        <ImageSection
-          src="/img/done_list.png"
-          className={`w-[120px] h-[120px] absolute top-[448px] left-[128px] sm:w-[240px] sm:h-[240px] sm:top-[624px] sm:left-[252px]
-            ${todo.length > 0 ? "hidden" : ""}`}
-            // todo 대신 done으로 변경 필요
-        />
-        <TextSection
+        {/* Done 그룹 */}
+        <div className="mt-[48px]">
+          <ImageSection src="/img/done.png"
+            className="w-[97px] h-[36px]"/>
+          <ImageSection src = "img/done_list.png" 
+            className="w-[120px] h-[120px] ml-[112px] sm:w-[240px] sm:h-[240px] sm:ml-[228px] sm:mt-[24px]"/>
+          <TextSection
           text={
             <>
             아직 다 한 일이 없어요.<br/>해야 할 일을 체크해보세요!
             </>
           }
-          className="text-slate-400 text-base text-center absolute w-[201px] h-[36px] top-[584px] left-[99px] sm:top-[888px] sm:left-[284px] lg:top-[544px] lg:left-[1178px]"
-          // todo 대신 done으로 변경 필요
-          isEmpty={todo.length === 0}/>
+          className="text-slate-400 text-base text-center ml-[83px] mt-[16px] sm:ml-[260px] sm:mt-[24px]"
+          isEmpty={done === 0 && todo.length === 0}/>
+        </div>
       </div>
 
-      {/* DO IT 리스트 - 데스크탑에서만 보임 */}
-      <div className="lg:block hidden">
-        <ImageSection
-            src="/img/done.png"
-            className="w-[97px] h-[36px] absolute left-[972px] top-[180px]"
-            />
-        <div className="flex flex-row mt-[64px] relative">
-            <div className="flex flex-col ml-[534px]">
-              <ImageSection
-                src="/img/doit_list.png"
-                className={`w-[240px] h-[240px]
-                  ${todo.length > 0 ? "hidden" : ""}`}
-                  />
-              <TextSection
-                text= {
-                  <div>
-                  할 일이 없어요.<br/>
-                  TODO를 새롭게 추가해주세요!
-                  </div>
-                }
-                className="text-slate-400 text-base text-center mt-[16px]"
-                isEmpty={todo.length === 0}/>
+      {/* 데스크탑에서 보기 */}
+      <div className="flex flex-row lg:block-hidden">
+        {/* Todo 그룹 */}
+        <div className="mt-[120px]">
+          <ImageSection src="/img/todo.png"
+            className="w-[101px] h-[36px] ml-[360px]"/>
+          <div className="w-[334px] sm:w-[696px] lg:w-[588px]">
+            {/* <CheckListForm todo={todo} doneChange={handleDone}/> */}
           </div>
-
-          <div className="flex flex-col items-center ml-[372px]">
-          <ImageSection
-            src="/img/done_list.png"
-            className={`w-[240px] h-[240px] ${todo.length > 0 ? "hidden" : ""}`}
-            />
+          <ImageSection src = "img/doit_list.png" 
+            className="w-[240px] h-[240px] ml-[534px] mt-[64px]"/>
           <TextSection
-            text={
+            text= {
               <div>
-              아직 다 한 일이 없어요.<br/>
-              해야 할 일을 체크해보세요!
+              할 일이 없어요.<br/>
+              TODO를 새롭게 추가해주세요!
               </div>
             }
-            className="text-slate-400 text-base text-center mt-[16px]"
-            // todo 대신 done으로 변경 필요
-            isEmpty={todo.length === 0}/>
-            </div>
-          </div>
+            className="text-slate-400 text-base text-center mt-[24px] ml-[553px]"
+            isEmpty={todo.length === 0 && done === 0}/>
+        </div>
+        
+        <div className="w-[198px]">
+        </div>
+        
+        {/* Done 그룹 */}
+        <div className="mt-[120px]">
+          <ImageSection src="/img/done.png"
+            className="w-[97px] h-[36px]"/>
+          <ImageSection src = "img/done_list.png" 
+            className="w-[240px] h-[240px] mt-[64px] ml-[174px]"/>
+          <TextSection
+          text={
+            <>
+            아직 다 한 일이 없어요.<br/>해야 할 일을 체크해보세요!
+            </>
+          }
+          className="text-slate-400 text-base text-center ml-[206px] mt-[24px]"
+          isEmpty={done === 0 && todo.length === 0}/>
+        </div>
       </div>
     </main>
   );
